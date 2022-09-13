c09ca06c6b49a35d55ff01596b821c01172f9cf3
Mikhail Yuzhakov <miha.yuzhakov@gmail.com>
2022-09-13 23:16:13 +0300

    Хайнойская башня и пирамидальная сортировка

---
+++ JavaS_3/hanoi_tower.java
@@ -0,0 +1,9 @@
+/*
+Реализовать алгоритм игры Ханойская башня
+*/
+
+public class hanoi_tower {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ JavaS_3/heap_sort.java
@@ -0,0 +1,5 @@
+public class heap_sort {
+    public static void main(String[] args) {
+        
+    }
+}
