63bae7f39d694613591e202b09c87ae37b84b4ac
Mikhail Yuzhakov <miha.yuzhakov@gmail.com>
2022-09-16 07:16:20 +0300

    v2

--- JavaS_3/hanoi_tower.java
+++ JavaS_3/hanoi_tower.java
@@ -10,7 +10,7 @@ public class hanoi_tower {
             play_field[i][0] = (byte)(i+1); //блины кладем на 1-ую ось
         }
         print_play_field(play_field);
-        find_solve(play_field, 1, play_field.length - 1, 0, 2); //блины перекладываем на 2-ую ось (0 индексирование)
+        find_solve(play_field, 1, play_field.length, 0, 2); //блины перекладываем на 2-ую ось (0 индексирование)
         print_play_field(play_field);
     }
 
@@ -29,13 +29,12 @@ public class hanoi_tower {
     //поиск и печать решения
     public static void find_solve(byte[][] map, int target_axis, int d, int cur_axis, int empty_axis) {
         int sum = 0;
-        d = d - 1;
-        for (int i = d; i >= 0; i--) {
-            map[i + 1][empty_axis] = map[i][cur_axis];
-            map[i][cur_axis] = 0;
+        for (int i = d - 1; i > 0; i--) { //переписываем все верхние блины на свободную ось
+            map[i][empty_axis] = map[i - 1][cur_axis];
+            map[i - 1][cur_axis] = 0;
         }
-        map[d][target_axis] = map[d][cur_axis];
-        map[d][cur_axis] = 0;
+        map[d - 1][target_axis] = map[d - 1][cur_axis]; //перекидываем самый большой блин на нужную ось
+        map[d - 1][cur_axis] = 0;
 
         //ищем пустую ось
         for (int i = 0; i < map.length; i++) sum = sum + map[i][0];
@@ -53,12 +52,27 @@ public class hanoi_tower {
         //определяем значение оставшейся оси
         if ((target_axis == 0 && empty_axis == 1) || (target_axis == 1 && empty_axis == 0)) cur_axis = 2;
         if ((target_axis == 0 && empty_axis == 2) || (target_axis == 2 && empty_axis == 0)) cur_axis = 1;
-        if ((target_axis == 1 && empty_axis == 2) || (target_axis == 2 && empty_axis == 1)) cur_axis = 3;
+        if ((target_axis == 1 && empty_axis == 2) || (target_axis == 2 && empty_axis == 1)) cur_axis = 0;
+        if (map[d - 1][0] == d + 1) cur_axis = 0;
+        if (map[d - 1][1] == d + 1) cur_axis = 1;
+        if (map[d - 1][2] == d + 1) cur_axis = 2;
 
-        System.out.printf("d = %d, cur_axis = %d, empty_axis = %d\n", d, cur_axis, empty_axis);
+        System.out.printf("d = %d, target_axis = %d, cur_axis = %d, empty_axis = %d\n", d, target_axis, cur_axis, empty_axis);
+        d--;
+        
+        for (int i = 0; i < map.length; i++)
+        {
+            for (int j = 0; j < map[0].length; j++) {
+                System.out.printf("%d  |  ", map[i][j]); 
+            }
+            System.out.println();
+        }
+        System.out.println("-----------------");
 
+        if (d > 1) find_solve(map, target_axis, d, cur_axis, empty_axis);
+        else return;
+            
         // if (map[0][target_axis] == 0) find_solve(map, target_axis, d, cur_axis, empty_axis);
         // else return;
-        return;
     }
 }
