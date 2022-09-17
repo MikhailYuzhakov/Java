7a24d4c7d38e44c9f659299df9793844e8676a9b
Mikhail Yuzhakov <miha.yuzhakov@gmail.com>
2022-09-17 10:05:23 +0300

    Ханойская башня и пирамидальная сортировка

--- JavaS_3/hanoi_tower.java
+++ JavaS_3/hanoi_tower.java
@@ -1,78 +1,20 @@
-/*
-Реализовать алгоритм игры Ханойская башня
-*/
-
+/**
+ * hanoi_tower
+ */
 public class hanoi_tower {
+
     public static void main(String[] args) {
-        byte N = 5; //количество дисков
-        byte[][] play_field = new byte[N][3]; //величина элемента массива это размер диска, три колонки это три оси
-        for (int i = 0; i < play_field.length; i++) { //"надеваем диски на одну из осей"
-            play_field[i][0] = (byte)(i+1); //блины кладем на 1-ую ось
-        }
-        print_play_field(play_field);
-        find_solve(play_field, 1, play_field.length, 0, 2); //блины перекладываем на 2-ую ось (0 индексирование)
-        print_play_field(play_field);
+        hanoi(4, 1, 3); //всего 5 колец, переносим с 1ой оси на 3ю ось
     }
 
-    //печать игрового поля
-    public static void print_play_field(byte[][] map) {
-        for (int i = 0; i < map.length; i++)
-        {
-            for (int j = 0; j < map[0].length; j++) {
-                System.out.printf("%d  |  ", map[i][j]); 
-            }
-            System.out.println();
+    public static void hanoi(int n, int i, int k) { //n - количество дисков, i - откуда переносим, k - куда переносим
+        if (n == 1)
+            System.out.printf("1: %d -> %d\n", i, k); //граничный случай, переносим первый диск последним шагом
+        else {
+            int temp = 6 - i - k; //вычисляем номер вспомогательной оси
+            hanoi(n - 1, i, temp); //перемещаем из i оси на временную ось все оставшиеся n-1 дисков
+            System.out.printf("%d: %d -> %d\n", n, i, k); //показываем какой диск и куда переносим
+            hanoi(n - 1, temp, k); //переносим диски из временной оси на целевую
         }
-        System.out.println("-----------------");
-    }
-
-    //поиск и печать решения
-    public static void find_solve(byte[][] map, int target_axis, int d, int cur_axis, int empty_axis) {
-        int sum = 0;
-        for (int i = d - 1; i > 0; i--) { //переписываем все верхние блины на свободную ось
-            map[i][empty_axis] = map[i - 1][cur_axis];
-            map[i - 1][cur_axis] = 0;
-        }
-        map[d - 1][target_axis] = map[d - 1][cur_axis]; //перекидываем самый большой блин на нужную ось
-        map[d - 1][cur_axis] = 0;
-
-        //ищем пустую ось
-        for (int i = 0; i < map.length; i++) sum = sum + map[i][0];
-        if (sum == 0) empty_axis = 0;
-        sum = 0;
-
-        for (int i = 0; i < map.length; i++) sum = sum + map[i][1];
-        if (sum == 0) empty_axis = 1;
-        sum = 0;
-
-        for (int i = 0; i < map.length; i++) sum = sum + map[i][2];
-        if (sum == 0) empty_axis = 2;
-        sum = 0;
-
-        //определяем значение оставшейся оси
-        if ((target_axis == 0 && empty_axis == 1) || (target_axis == 1 && empty_axis == 0)) cur_axis = 2;
-        if ((target_axis == 0 && empty_axis == 2) || (target_axis == 2 && empty_axis == 0)) cur_axis = 1;
-        if ((target_axis == 1 && empty_axis == 2) || (target_axis == 2 && empty_axis == 1)) cur_axis = 0;
-        if (map[d - 1][0] == d + 1) cur_axis = 0;
-        if (map[d - 1][1] == d + 1) cur_axis = 1;
-        if (map[d - 1][2] == d + 1) cur_axis = 2;
-
-        System.out.printf("d = %d, target_axis = %d, cur_axis = %d, empty_axis = %d\n", d, target_axis, cur_axis, empty_axis);
-        d--;
-        
-        for (int i = 0; i < map.length; i++)
-        {
-            for (int j = 0; j < map[0].length; j++) {
-                System.out.printf("%d  |  ", map[i][j]); 
-            }
-            System.out.println();
-        }
-        System.out.println("-----------------");
-
-        if (d > 1) find_solve(map, target_axis, d, cur_axis, empty_axis);
-        else return;
-            
-        // if (map[0][target_axis] == 0) find_solve(map, target_axis, d, cur_axis, empty_axis);
-        // else return;
     }
 }
--- JavaS_3/heap_sort.java
+++ JavaS_3/heap_sort.java
@@ -1,5 +1,85 @@
+/*
+ * отсортировать массив с помощью heapsort (пираимдальная сортировка)
+ * дочерний элемент всегда меньше родительского (используем бинарное дерево)
+ */
+
+import java.util.Random;
+import java.util.TreeSet;
+
 public class heap_sort {
     public static void main(String[] args) {
-        
+
+        TreeSet<Integer> ts = new TreeSet<Integer>(); //встроенная коллекция Java дерево, сортирует по возрастанию
+        int[] array = gen_array(10);
+        for (int item : array) { //записываем в коллекцию все значения массива
+            ts.add(item);
+        }
+        System.out.println("Заданный массив: " + print_arr(array)); //изначальный массив
+        heapSort(array); //сортируем массив с помощью heap sort
+        System.out.println("Отсортированный массив: " + print_arr(array)); //отсортированный массив
+        System.out.println("Отсортированный через TreeSet массив: " + ts); //отсортированный массив с помощью коллекции 
+        //TreeSet не хранит повторяющиеся элементы
+    }
+
+    //случайно генерирует массив
+    public static int[] gen_array(int N) {
+        Random random = new Random();
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = random.nextInt(-100, 100);
+        }
+        return arr;
+    }
+
+    //печатает массив
+    public static StringBuilder print_arr(int[] arr) {
+        StringBuilder str = new StringBuilder();
+        for (int item : arr) {
+            str.append(item);
+            str.append(" ");
+       } 
+       return str;
+    }
+
+    //пирамидальная сортировка массива
+    public static void heapSort(int[] array) {
+        int n = array.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--) { //создаем дерево
+            heapify(array, i, n); //ставит больший элемент на место родительского и обновляет дерево
+        }
+
+        //с конца восстанавливаем массив согласно дереву (максимальный элемент - корень дерева - в конец массива)
+        for (int i = n - 1; i >= 0; i--) {
+           int temp = array[i];
+           array[i] = array[0];
+           array[0] = temp;
+           
+           heapify(array, 0, i); //обновляем дерево
+        }     
+    }
+
+    //обновление бинарного дерева
+    public static void heapify(int[] array, int i, int n) {
+        int l = i * 2 + 1; //левый ребенок
+        int r = i * 2 + 2; //правый ребенок
+        int max = i; //полагаем, что по умолчанию родительский элемент - максимальный
+
+        //если индекс левого "ребенка" не вылезает на пределы массива и он больше родительского, то
+        if (l < n && array[l] > array[max]) {
+            max = l; //запоминаем индекс максмального элемента тройки
+        }
+        //аналогично
+        if (r < n && array[r] > array[max]) {
+            max = r;
+        }
+        //если один из детей больше родителя, то
+        if (i != max) {
+            int temp = array[i]; //меняем большоего ребенка местами с родителем
+            array[i] = array[max];
+            array[max] = temp;
+
+            heapify(array, max, n); //рекурсивно запускам функцию, чтобы обновить дерево при изменении родителя
+        }
     }
 }
