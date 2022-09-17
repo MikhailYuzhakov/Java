5ad772d2e39f365b9c51aab96878b2f808b5fdd4
Mikhail Yuzhakov <miha.yuzhakov@gmail.com>
2022-09-17 20:05:23 +0300

    версия после рефакторинга

--- JavaS_2/N_triangle.java
+++ JavaS_2/N_triangle.java
@@ -1,4 +1,5 @@
 /*
+РЕФАКТОРИНГ (исправленные функции помечены *)
 Написать программу вычисления n-ого треугольного числа.
 */
 public class N_triangle {
@@ -6,9 +7,10 @@ public class N_triangle {
     static int [] triang = new int[M]; //массив, хранящий значения треугольника
     public static void main(String[] args) {
         triangle(0, M); //рекурсивно вычисляет числа в треугольнике
-        printArr1D(triang, triang.length); //печать одномерного массива
+        System.out.println(arrToStr(triang, triang.length)); //печать одномерного массива
     }
 
+    //считаем числа
     public static void triangle(int n, int M) {
         triang[n] = (int) (0.5 * n * (n + 1)); //вычисляем по известной формуле
         n = n + 1;
@@ -16,11 +18,12 @@ public class N_triangle {
         else triangle(n, M); //вызываем рекурсивно ту же функцию для дальнейшего расчета 
     }
 
-    // для печать одномерного массива
-    public static void printArr1D(int[] arr, int length) {
-        System.out.print("{ ");
-        for (int i = 1; i < length; i++)
-            System.out.print(arr[i] + " ");
-        System.out.println("}");
+    // *выполняет только 1 действие - переносит массив длины length в строку
+    public static String arrToStr(int[] arr, int length) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        for (int i = 1; i < length; i++) sb.append(arr[i] + " ");
+        sb.append("}");
+        return sb.toString();
     }
 }
